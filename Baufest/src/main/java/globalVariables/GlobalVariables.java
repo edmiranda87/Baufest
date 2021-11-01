50ac1d962fbcc7be2ff5a1ae43dad7d9f7ee3259
jose.miranda <jose.miranda@DF-1418-01.sieenasoftware.com>
2021-11-01 11:12:45 -0600

    second commit

--- Baufest/src/main/java/globalVariables/GlobalVariables.java
+++ Baufest/src/main/java/globalVariables/GlobalVariables.java
@@ -3,7 +3,7 @@ package globalVariables;
 public class GlobalVariables {
 	
 	public static final String HOME_PAGE = "https://www.demoblaze.com/";
-	public static final String USDER_ADMIN = "NewTest1";
+	public static final String USDER_ADMIN = "NewTestA";
 	public static final String PASSWORD_ADMIN = "admin123";
 
 }
